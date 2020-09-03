8e34963052cecdede895f3d254923d65dfd820c6
Vlad Voinov <voinovvladv@gmail.com>
2020-09-03 14:03:17 +0200

    master: Added basic UI;

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,11 +11,11 @@
 
 //==============================================================================
 GGranulaAudioProcessorEditor::GGranulaAudioProcessorEditor (GGranulaAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p),
+        audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (268, 268);
+    addAndMakeVisible(main_panel);
+    setSize(720, 200);
 }
 
 GGranulaAudioProcessorEditor::~GGranulaAudioProcessorEditor()
@@ -24,23 +24,18 @@ GGranulaAudioProcessorEditor::~GGranulaAudioProcessorEditor()
 
 //==============================================================================
 void GGranulaAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Da, eto jestko", getLocalBounds(), juce::Justification::centred, 1);
-    
-    //Image background = ImageCache::getFromMemory(BinaryData::krug_jpg, BinaryData::krug_jpgSize);
-    Image background = ImageCache::getFromMemory(BinaryData::BroVoging_jpg, BinaryData::BroVoging_jpgSize);
-    g.drawImageAt (background, 0, 0);
-}
+{}
 
 void GGranulaAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    juce::Grid grid;
+    using Track = juce::Grid::TrackInfo;
+    using Fr = juce::Grid::Fr;
+    using Item = juce::GridItem;
+    grid.templateRows    = { Track (Fr (1)) };
+    grid.templateColumns = { Track (Fr (1)) };
+    grid.items = { Item(main_panel) };
+    grid.performLayout (getLocalBounds());
 }
 
 bool GGranulaAudioProcessorEditor::isInterestedInFileDrag(const StringArray& files)
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -31,6 +31,397 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     GGranulaAudioProcessor& audioProcessor;
+    
+    //==============================================================================
+    struct BaseComponent : public Component
+    {
+        using Colour    = juce::Colour;
+        using FontStyle = Font::FontStyleFlags;
+        using Track     = juce::Grid::TrackInfo;
+        using Fr        = juce::Grid::Fr;
+        using Item      = juce::GridItem;
+        using Margin    = juce::GridItem::Margin;
+        using Justify   = juce::GridItem::JustifySelf;
+        using Align     = juce::GridItem::AlignSelf;
+        
+        //==============================================================================
+        BaseComponent(Colour background) : background_colour(background) {}
+        
+        //==============================================================================
+        void paint (juce::Graphics& g) override { g.fillAll(background_colour); }
+        void resized() override {}
+        
+        //==============================================================================
+        struct PanelNameFont
+        {
+            const juce::String   name   { "Helvetica" };
+            float                size   { 15 };
+            Font::FontStyleFlags style  { FontStyle::bold };
+            juce::Colour         colour { juce::Colours::black };
+        };
+        struct ParameterLabelFont
+        {
+            const juce::String   name   { "Helvetica" };
+            float                size   { 13 };
+            Font::FontStyleFlags style  { FontStyle::plain };
+            juce::Colour         colour { juce::Colours::black };
+        };
+        
+        //==============================================================================
+        PanelNameFont      getPanelNameFont      () { return PanelNameFont(); }
+        ParameterLabelFont getParameterLabelFont () { return ParameterLabelFont(); }
+        
+        //==============================================================================
+        Colour background_colour;
+    };
+    
+    //==============================================================================
+    struct TransposePanel : public BaseComponent
+    {
+        TransposePanel(juce::Colour background):
+            BaseComponent(background),
+            combo_box("transpose")
+        {
+            auto font = getParameterLabelFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            addAndMakeVisible(label);
+            
+            combo_box.addItem("-2", 1);
+            combo_box.addItem("-1", 2);
+            combo_box.addItem("0", 3);
+            combo_box.addItem("+1", 4);
+            combo_box.addItem("+2", 5);
+            combo_box.setSelectedId(3);
+            addAndMakeVisible(combo_box);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label),
+                Item(combo_box),
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::ComboBox combo_box;
+        juce::Label    label { "transpose", "Transpose" };
+    };
+    
+    //==============================================================================
+    struct WaveformPanel : public BaseComponent
+    {
+        WaveformPanel(juce::Colour background):
+            BaseComponent(background),
+            combo_box("waveform")
+        {
+            auto font = getParameterLabelFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            addAndMakeVisible(label);
+            
+            combo_box.addItem("sin", 1);
+            combo_box.addItem("saw", 2);
+            combo_box.setSelectedId(1);
+            addAndMakeVisible(combo_box);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label),
+                Item(combo_box),
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::ComboBox combo_box;
+        juce::Label    label { "waveform", "Waveform" };
+    };
+    
+    //==============================================================================
+    struct OscillatorPanel : public BaseComponent
+    {
+        OscillatorPanel(juce::Colour background, const juce::String& label_text) :
+            BaseComponent(background),
+            label({}, label_text),
+            transpose_panel(background),
+            waveform_panel(background)
+        {
+            auto font = getParameterLabelFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            addAndMakeVisible(label);
+            addAndMakeVisible(transpose_panel);
+            addAndMakeVisible(waveform_panel);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (2)) };
+            grid.templateColumns = { Track (Fr (1)), Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label),
+                Item(),
+                Item(transpose_panel).withMargin(Margin(0, 50, 0, 0)),
+                Item(waveform_panel).withMargin(Margin(0, 50, 0, 0)),
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::Label    label;
+        TransposePanel transpose_panel;
+        WaveformPanel  waveform_panel;
+    };
+    
+    //==============================================================================
+    struct OscillatorsPanel : public BaseComponent
+    {
+        OscillatorsPanel(juce::Colour background) :
+            BaseComponent(background),
+            osc_1_panel(background, "OSC #1"),
+            osc_2_panel(background, "OSC #2")
+        {
+            auto font = getPanelNameFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            addAndMakeVisible(label);
+            addAndMakeVisible(osc_1_panel);
+            addAndMakeVisible(osc_2_panel);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (2)), Track (Fr (2)) };
+            grid.templateColumns = { Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label).withMargin(10),
+                Item(osc_1_panel).withMargin(10),
+                Item(osc_2_panel).withMargin(10)
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::Label     label { "oscillators", "Oscillators" };
+        OscillatorPanel osc_1_panel;
+        OscillatorPanel osc_2_panel;
+    };
+    
+    //==============================================================================
+    struct CutoffPanel : public BaseComponent
+    {
+        CutoffPanel(juce::Colour background) :
+            BaseComponent(background),
+            cutoff("Cutoff")
+        {
+            auto font = getPanelNameFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            label.setJustificationType(Justification::topLeft);
+            addAndMakeVisible(label);
+            
+            cutoff.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            cutoff.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50.0f, 20.0f);
+            cutoff.setSkewFactor(0.25f);
+            cutoff.setRange(Range<double>(10.0f, 16000.0f), 1.0f);
+            addAndMakeVisible(cutoff);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label),
+                Item(cutoff)
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::Label  label { "cutoff", "Cutoff" };
+        juce::Slider cutoff;
+    };
+    
+    //==============================================================================
+    struct FilterPanel : public BaseComponent
+    {
+        FilterPanel(juce::Colour background) : BaseComponent(background)
+        {
+            auto font = getPanelNameFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            // label.setJustificationType(Justification::topLeft);
+            addAndMakeVisible(label);
+            
+            cutoff.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            cutoff.setTextBoxStyle(juce::Slider::TextBoxRight, true, 50, 30);
+            cutoff.setSkewFactor(0.25f);
+            cutoff.setRange(Range<double>(10.0f, 16000.0f), 1.0f);
+            addAndMakeVisible(cutoff);
+            
+            q.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            q.setTextBoxStyle(juce::Slider::TextBoxRight, true, 35.0f, 20.0f);
+            q.setSkewFactor(0.5f);
+            q.setRange(Range<double>(0.1f, 1.2f), 0.1f);
+            addAndMakeVisible(q);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (2)) };
+            grid.templateColumns = { Track (Fr (1)), Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label).withMargin(8),
+                Item(),
+                Item(cutoff),
+                Item(q).withMargin(Margin(0, 20, 0, 0))
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        juce::Label  label  { "filter", "Filter" };
+        juce::Slider cutoff { "cutoff" };
+        juce::Slider q      { "q" };
+    };
+    
+    //==============================================================================
+    struct ADSRPanel : public BaseComponent
+    {
+        ADSRPanel(juce::Colour background) : BaseComponent(background)
+        {
+            attack.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            attack.setTextBoxStyle(juce::Slider::TextBoxRight, true, 30, 16);
+            attack.setNormalisableRange(juce::NormalisableRange<double>(0.01f, 10.0f, 0.01f, 0.5f));
+            addAndMakeVisible(attack);
+            
+            decay.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            decay.setTextBoxStyle(juce::Slider::TextBoxRight, true, 30, 16);
+            decay.setNormalisableRange(juce::NormalisableRange<double>(0.01f, 10.0f, 0.01f, 0.5f));
+            addAndMakeVisible(decay);
+            
+            sustain.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            sustain.setTextBoxStyle(juce::Slider::TextBoxRight, true, 30, 16);
+            sustain.setNormalisableRange(juce::NormalisableRange<double>(0.01f, 10.0f, 0.01f, 0.5f));
+            addAndMakeVisible(sustain);
+            
+            release.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+            release.setTextBoxStyle(juce::Slider::TextBoxRight, true, 30, 16);
+            release.setNormalisableRange(juce::NormalisableRange<double>(0.01f, 20.0f, 0.01f, 0.5f));
+            addAndMakeVisible(release);
+        }
+        
+        void resized() override
+        {
+           juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)), Track (Fr (1)), Track (Fr (1)), Track (Fr (1)) };
+            grid.items =
+            {
+                Item(attack),
+                Item(decay),
+                Item(sustain),
+                Item(release)
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        
+        juce::Slider attack  { "attack" };
+        juce::Slider decay   { "decay" };
+        juce::Slider sustain { "sustain" };
+        juce::Slider release { "release" };
+    };
+    
+    //==============================================================================
+    struct AmpPanel : public BaseComponent
+    {
+        AmpPanel(juce::Colour background) :
+            BaseComponent(background),
+            amp_panel(background)
+        {
+            auto font = getPanelNameFont();
+            label.setColour(juce::Label::ColourIds::textColourId, font.colour);
+            label.setFont(Font(font.name, font.size, font.style));
+            addAndMakeVisible(label);
+            addAndMakeVisible(amp_panel);
+        }
+        
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (2)) };
+            grid.templateColumns = { Track (Fr (1)) };
+            grid.items =
+            {
+                Item(label).withMargin(8),
+                Item(amp_panel)
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        
+        juce::Label label { "amp", "AMP" };
+        ADSRPanel   amp_panel;
+    };
+    
+    //==============================================================================
+    struct FilterAmpPanel : public BaseComponent
+    {
+        FilterAmpPanel(juce::Colour filter_background, juce::Colour adsr_background) :
+            BaseComponent(filter_background),
+            filter_panel(filter_background),
+            amp_panel(adsr_background)
+        {
+            addAndMakeVisible(filter_panel);
+            addAndMakeVisible(amp_panel);
+        }
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)), Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)), };
+            grid.items =
+            {
+                Item(filter_panel),
+                Item(amp_panel)
+            };
+            grid.performLayout(getLocalBounds());
+        }
+        FilterPanel filter_panel;
+        AmpPanel    amp_panel;
+    };
+    
+    //==============================================================================
+    struct MainPanel : public BaseComponent
+    {
+        MainPanel() :
+            BaseComponent(juce::Colours::grey),
+            osc_panel(juce::Colours::silver),
+            filter_adsr_panel(juce::Colours::silver, juce::Colours::silver)
+        {
+            addAndMakeVisible(osc_panel);
+            addAndMakeVisible(filter_adsr_panel);
+        }
+        
+        //==============================================================================
+        void resized() override
+        {
+            juce::Grid grid;
+            grid.templateRows    = { Track (Fr (1)) };
+            grid.templateColumns = { Track (Fr (1)), Track (Fr (1)) };
+            grid.items = { Item(osc_panel).withMargin(10), Item(filter_adsr_panel).withMargin(10) };
+            grid.performLayout (getLocalBounds());
+        }
+        
+        //==============================================================================
+        OscillatorsPanel osc_panel;
+        FilterAmpPanel  filter_adsr_panel;
+    };
+    
+    MainPanel main_panel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GGranulaAudioProcessorEditor)
 };
